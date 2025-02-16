565a640ed7dfc2099de5b2990f4ce655028ac20f
rafiafsan <rafiafs45@gmail.com>
2025-02-16 13:33:44 +0600

    fs

---
+++ lib/expensetracker.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class ExpenseTracker extends StatefulWidget {
+  const ExpenseTracker({super.key});
+
+  @override
+  State<ExpenseTracker> createState() => _ExpenseTrackerState();
+}
+
+class _ExpenseTrackerState extends State<ExpenseTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
