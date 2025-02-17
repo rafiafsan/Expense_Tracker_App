2deb8681fc71e16203eb3a932a69a53c85fbb26e
rafiafsan <rafiafs45@gmail.com>
2025-02-17 12:05:14 +0600

    fs

--- lib/expensetracker.dart
+++ lib/expensetracker.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 import 'model/expense.dart';
 
 class ExpenseTracker extends StatefulWidget {
@@ -12,83 +11,84 @@ class ExpenseTracker extends StatefulWidget {
 
 class _ExpenseTrackerState extends State<ExpenseTracker> {
   final List<Expense> _expense = [];
-  final List<String> _category = [
+  final List<String> _categories = [
     'Food',
     'Transport',
     'Entertainment',
     'Bills'
   ];
+
   double _total = 0.0;
 
-  void _addExpense(String title, double amount, DateTime date, String category){
+  void _addExpense(String title,double amount, DateTime date,String category){
     setState(() {
       _expense.add(Expense(title: title, amount: amount, date: date, category: category));
-      _total+=amount;
+      _total += amount;
+    });
+
+  }
+
+  void _deleteExpense(int index){
+    setState(() {
+      _total -= _expense[index].amount;
+      _expense.removeAt(index);
     });
   }
 
+
+
+
   void _showForm(BuildContext context) {
-    TextEditingController titlecontroller = TextEditingController();
-    TextEditingController amountcontroller = TextEditingController();
-
-    String selectedCategory = _category.first;
+    TextEditingController titleController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+    String selectedCategory = _categories.first;
     DateTime selectedDate = DateTime.now();
-
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
         builder: (_) {
           return Padding(
             padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-              left: 16,
-              right: 16,
-              top: 16,
-            ),
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+                left: 16,
+                right: 16,
+                top: 16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
-                  controller: titlecontroller,
+                  controller: titleController,
                   decoration: InputDecoration(labelText: 'Title'),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
+                SizedBox(height: 10,),
                 TextField(
-                  controller: amountcontroller,
+                  controller: amountController,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(labelText: 'Amount'),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                DropdownButtonFormField(
-                  items: _category
-                      .map((category) => DropdownMenuItem(
-                          value: category, child: Text(category)))
-                      .toList(),
+                SizedBox(height: 10,),
+                DropdownButtonFormField<String>(
+                  items: _categories.map((category)=>DropdownMenuItem(value : category , child: Text(category))).toList(),
                   onChanged: (value) => selectedCategory = value!,
-                  decoration: InputDecoration(labelText: "Category"),
+                  decoration: InputDecoration(labelText: 'Category'),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
+                SizedBox(height: 20,),
                 SizedBox(
                     width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          if(titlecontroller.text.isEmpty || double.tryParse(amountcontroller.text)==null){
-                            _addExpense(titlecontroller.text, double.parse(amountcontroller.text), selectedDate , selectedCategory);
-                            return;
-                          }
-                          titlecontroller.clear();
-                          amountcontroller.clear();
-                          Navigator.pop(context);
-                        }, child: Text("Add Expense"))),
-                SizedBox(
-                  height: 20,
-                )
+                    child: ElevatedButton(onPressed: (){
+                      if(titleController.text.isEmpty || double.tryParse(amountController.text) == null ){
+                        return;
+                      }
+                      _addExpense(titleController.text, double.parse(amountController.text), selectedDate, selectedCategory);
+                      titleController.clear();
+                      amountController.clear();
+                      Navigator.pop(context);
+
+
+
+                    }, child: Text("Add Expense"))),
+                SizedBox(height: 20,)
+
               ],
             ),
           );
@@ -99,11 +99,7 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Expense Tracker App',
-          style: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
+        title: Text("Expense Tracker"),
         actions: [
           IconButton(onPressed: () => _showForm(context), icon: Icon(Icons.add))
         ],
@@ -112,14 +108,14 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
         children: [
           Center(
             child: Card(
-              margin: EdgeInsets.all(16.0),
+              margin: EdgeInsets.all(20),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12)),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(30.0),
                 child: Text(
                   "Total: \$${_total}",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
@@ -128,14 +124,21 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
             child: ListView.builder(
                 itemCount: _expense.length,
                 itemBuilder: (ctx, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        child: Text(_expense[index].category),
+                  return Dismissible(
+                    key: Key(_expense[index].hashCode.toString()),
+                    background: Container(color: Colors.red),
+                    onDismissed: (direction)=>_deleteExpense(index),
+                    child: Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.blueAccent,
+                          child: Text(_expense[index].category),
+                        ),
+                        title: Text(_expense[index].title),
+                        subtitle: Text(
+                            DateFormat.yMMMd().format(_expense[index].date)),
+                        trailing: Text(_expense[index].amount.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
                       ),
-                      title: Text(_expense[index].title),
-                      subtitle: Text(_expense[index].date.toString()),
                     ),
                   );
                 }),
